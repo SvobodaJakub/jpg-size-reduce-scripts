e0c1065cc59ebf8affe4606f9c55ffd31c008c46
Jakub Svoboda <SvobodaJakub@users.noreply.github.com>
2018-10-22 07:44:38 +0200

    Added a check to ensure the file size is indeed reduced.

--- jpg-quality-reducer-hiquality-9600-90.sh
+++ jpg-quality-reducer-hiquality-9600-90.sh
@@ -4,6 +4,19 @@
 # run like this:
 # find . -type d -exec bash -c 'cd "$1" && pwd && bash /path/to/jpg-quality-reducer-script.sh' _ {} ';'
 
+
+# exit on empty variables
+set -u
+
+# exit on non-zero status
+set -e
+
+# if there are no jpg files, the glob in the for loop is null and the loop doesn't run
+# (otherwise it would go on creating crazy directories)
+shopt -s nullglob
+
+
+
 resolution="9600x9600"
 quality="90%"
 
@@ -84,7 +97,30 @@ if [[ ! -f reduced_quality.txt ]]; then
     cat "$0" >> reduced_quality.txt
     nice -n 16 mogrify -define jpeg:q-table="${quantization_table}" -sampling-factor 4:2:2 -define jpeg:dct-method=float -quality "$quality" -resize "${resolution}"\> -filter Lanczos -interlace Plane -- *.[Jj][Pp]*[Gg] ;
     rm -f -- *.[Jj][Pp]*[Gg]~* ;
+    for f in *.[Jj][Pp]*[Gg]
+    do
+        echo -n "${f}"
+        if [[ ! -f "${f}" ]]  # don't care about directories or non-regular files, or when nullglob doesn't work
+        then
+            echo ": file doesn't exist, skipping"
+            continue
+        fi
+        if [[ ! -s "${f}" ]]
+        then
+            echo ": file has zero size, skipping"
+            continue
+        fi
+        touch -r "backup_before_reducing_quality/${f}" "${f}" # preserve timestamp
+        size_new=$(wc -c < "${f}")
+        size_orig=$(wc -c < "backup_before_reducing_quality/${f}")
+        if (( size_new > size_orig ))
+        then
+            echo "the orig file is smaller (${size_orig}) than the new one (${size_new})"
+            mv "backup_before_reducing_quality/${f}" "${f}"
+        fi
+    done
     rm -rf backup_before_reducing_quality;
+
 else
     echo "No action, quality already reduced."
 fi
